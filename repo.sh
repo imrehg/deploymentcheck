2ed13fd7063c28596515a7aabe55ceb481879876
Gergely Imreh <imrehg@gmail.com>
2020-05-15 20:21:23 +0100

    curl
    Signed-off-by: Gergely Imreh <imrehg@gmail.com>

--- repo.sh
+++ repo.sh
@@ -6,16 +6,17 @@ source ./env
 #      -H "Authorization: token $TOKEN" \
 #     https://api.github.com/repos/imrehg/deploymentcheck/deployments
 
+
+API_RESPONSE=$(curl --silent -H "Accept: application/vnd.github.ant-man-preview+json" -H "Authorization: token $TOKEN" -X POST -H 'Content-Type: application/json' -d '{"ref": "curl", "environment": "debug", "transient_environment": true, "description": "My debug env", "required_contexts": []}' https://api.github.com/repos/imrehg/deploymentcheck/deployments)
+echo $API_RESPONSE
+
+URL=$(echo $API_RESPONSE | jq ".statuses_url")
+echo $URL
+
+
+
 # curl -H "Accept: application/vnd.github.ant-man-preview+json" \
 #      -H "Authorization: token $TOKEN" \
 #      -X POST -H 'Content-Type: application/json' \
-#      -d '{"ref": "master", "environment": "debug", "transient_environment": true, "description": "My debug env", "required_contexts": []}' \
-#     https://api.github.com/repos/imrehg/deploymentcheck/deployments
-
-
-
-curl -H "Accept: application/vnd.github.ant-man-preview+json" \
-     -H "Authorization: token $TOKEN" \
-     -X POST -H 'Content-Type: application/json' \
-     -d '{"state": "success", "target_url": "https://gergely.imreh.net", "description": "My debug status", "environment_url": "https://twitter.com/imrehg"}' \
-     https://api.github.com/repos/imrehg/deploymentcheck/deployments/228789535/statuses
+#      -d '{"state": "success", "target_url": "https://gergely.imreh.net", "description": "My debug status", "environment_url": "https://twitter.com/imrehg"}' \
+#     https://api.github.com/repos/imrehg/deploymentcheck/deployments/228791476/statuses
